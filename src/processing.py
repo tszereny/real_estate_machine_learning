f6dd40a58c5a6ea074d4a56d371905f5fc9593a4
unknown <tibor.szerenyi@gmail.com>
2019-06-07 14:29:07 +0200

    Rewritten into transformers

--- src/processing.py
+++ src/processing.py
@@ -2,53 +2,38 @@ import re
 import pandas as pd, numpy as np
 import string
 from zlib import crc32
+from src.base import BaseTransformer
 import src.preparation as prep
 
-def generate_na(df, na_eqs):
-    gen_na=lambda x: None if x in na_eqs else x
-    if isinstance(df, pd.Series):
-        return df.apply(gen_na)
-    elif isinstance(df, pd.DataFrame):
-        return df.applymap(gen_na)
 
-def rename_cols_to_new(df):
-    OLD_NEW={'data_id':'property_id', 'description':'desc',
-    'district':'city_district','latitude': 'lat',
-    'link':'property_url', 'load_time':'timestamp',
-    'longitude': 'lng', 'max_list': 'max_listing',
-    'price':'price_in_huf', 'quadmeter': 'area_size',
-    'rooms': 'room', 'type': 'listing_type'}
+class ColumnRenamer(BaseTransformer):
 
-    df.columns=df.columns.str.lower()
-    return df.rename(columns=OLD_NEW)
+    def __init__(self, old_to_new=None, hun_to_eng=None):
+        self.old_to_new = old_to_new
+        self.hun_to_eng = hun_to_eng
 
-def rename_cols_to_eng(df):
-    HUN_ENG={'akadálymentesített':'accessibility', 'belmagasság':'ceiling_height', 'busz':'buses',
-    'busz_count':'buses_count', 'bútorozott':'furnished','dohányzás':'smoking', 'emelet':'floors',
-    'energiatanúsítvány': 'energy_perf_cert', 'erkély':'balcony','fürdő_és_wc':'bath_and_wc',
-    'fűtés':'type_of_heating', 'gépesített':'equipped', 'hajó':'boats', 'hajó_count': 'boats_count',
-    'hév':'local_railways','hév_count': 'local_railways_count', 'ingatlan_állapota':'condition_of_real_estate',
-    'kertkapcsolatos':'with_entry_to_garden', 'kilátás':'view','kisállat':'pets', 'komfort':'convenience_level',
-    'költözhető':'vacant', 'lakópark_neve':'residental_park_name','légkondicionáló': 'air_conditioned',
-    'metró':'metro_lines', 'metró_count':'metro_lines_count', 'min._bérleti_idő': 'min_tenancy','parkolás':'parking',
-    'parkolóhely_ára': 'parking_lot_price','rezsiköltség':'utilities','tetőtér':'attic',
-    'troli':'trolley_buses', 'troli_count':'trolley_buses_count','tájolás':'orientation',
-    'villamos':'trams','villamos_count':'trams_count','éjszakai':'all_night_services',
-    'éjszakai_count':'all_night_services_count','építés_éve': 'year_built','épület_szintjei':'building_floors'}
-    return df.rename(columns=HUN_ENG)
-   
-def translate_listing_type(df, col_n='listing_type'):
-    LISTING_HUN_ENG={'elado': 'for-sale', 'kiado': 'for-rent'}
-    df[col_n]=df[col_n].map(LISTING_HUN_ENG)
-    return df
-    
-def transform_naming(df):
-    df=rename_cols_to_new(df)
-    df=rename_cols_to_eng(df)
-    df.address=df.address.str.strip()
-    df.desc=generate_na(df.desc, na_eqs=['|   |', '| |', ' '])
-    df=translate_listing_type(df)
-    return df
+    def to_new(self, df):
+        df.columns = df.columns.str.lower()
+        return df.rename(columns=self.old_to_new)
+
+    def to_eng(self, df):
+        return df.rename(columns=self.hun_to_eng)
+
+    def transform(self, X):
+        X = X.copy()
+        X = self.to_new(X)
+        X = self.to_eng(X)
+        return X
+
+class Translator(BaseTransformer):
+
+    def __init__(self, column_name, hun_eng_map):
+        self.column_name = column_name
+        self.hun_eng_map = hun_eng_map
+
+    def transform(self, X):
+        X[self.column_name] = X[self.column_name].map(self.hun_eng_map)
+        return X
 
 def multiply(func, **kwargs):
     def func_wrapper(from_string, thousand_eq=None, million_eq=None, billion_eq=None, thousand_mlpr = 1e3, million_mlpr = 1e6, billion_mlpr = 1e9):
