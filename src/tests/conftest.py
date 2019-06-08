e87c03098c78234b6621e3be9b43df73aae0f59b
unknown <tibor.szerenyi@gmail.com>
2019-06-08 23:33:55 +0200

    Preprocessing tests added

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,7 @@
 from src.utils import RealEstateData
 import pytest
+from src.processing import ColumnRenamer, Translator
+from pipeline import OLD_TO_NEW, HUN_TO_ENG, LISTING_TYPE_HUN_TO_ENG
 import pandas as pd
 import os
 
@@ -10,4 +12,45 @@ def real_estate_data():
     DATA_FNAME = 'sample.csv'
     red = RealEstateData(FIXTURE_DIR, DATA_FNAME)
     return red
+
+
+@pytest.fixture(scope='module')
+def real_estate_raw(real_estate_data):
+
+    def _real_estate_raw(date_idx):
+        dates = real_estate_data.directories
+        return real_estate_data.read(dir_name='raw', date=dates[date_idx])
+
+    return _real_estate_raw
+
+@pytest.fixture(scope='module', params=[0, 1])
+def real_estate_renamed(real_estate_raw, request):
+    cr = ColumnRenamer(old_to_new=OLD_TO_NEW, hun_to_eng=HUN_TO_ENG)
+    df = cr.transform(real_estate_raw(request.param))
+    return df
+
+# class TestColumRenamer:
+#
+#     def test_transform(self, real_estate_data):
+#
+#         date_0 = cr.transform(dates[0]).columns.tolist()
+#         date_1 = cr.transform(dates[1]).columns.tolist()
+#         assert set(date_0) - set(date_1) == {'batch_num', 'is_ad_active'}
+#
+#
+# class TestTranslator:
+#
+#     @pytest.mark.parametrize('idx', [0, 1])
+#     def test_wrong_column(self, idx, real_estate_data):
+#         date = real_estate_data.directories[idx]
+#         cr = ColumnRenamer(old_to_new=OLD_TO_NEW, hun_to_eng=HUN_TO_ENG)
+#         df = cr.transform(real_estate_data.read(dir_name='raw', date=date))
+#         t = Translator(column_name='wrong_column', hun_eng_map={})
+#         with pytest.raises(KeyError):
+#             t.transform(df)
+#         to_be_translated = 'listing_type'
+#         t = Translator(column_name=to_be_translated, hun_eng_map=LISTING_TYPE_HUN_TO_ENG)
+#         translated_listing_type = t.transform(df)[to_be_translated]
+#         assert len(translated_listing_type.unique()) == 2
+#         assert translated_listing_type.apply(lambda s: s not in ('elado', 'kiado')).all() == True
 
--- src/tests/test_processing.py
+++ src/tests/test_processing.py
@@ -1,26 +1,35 @@
 import pytest
 from sklearn.pipeline import Pipeline
-from src.processing import ColumnRenamer, Translator
-from pipeline import OLD_TO_NEW, HUN_TO_ENG
+from src.processing import ColumnRenamer, Translator, StringStandardizer
+from pipeline import OLD_TO_NEW, HUN_TO_ENG, LISTING_TYPE_HUN_TO_ENG
 
+
+class TestStringStandardizer:
+
+    def test_transform(self, real_estate_raw):
+        df = real_estate_raw(0)
+        ss = StringStandardizer(func=lambda s: None)
+        assert ss.transform(df)[ss.column_names].isnull().all().all() == True
 
 class TestColumRenamer:
 
-    def test_transform(self, real_estate_data):
+    def test_transform(self, real_estate_raw):
         cr = ColumnRenamer(old_to_new=OLD_TO_NEW, hun_to_eng=HUN_TO_ENG)
-        dates = [real_estate_data.read(dir_name='raw', date=d) for d in real_estate_data.directories]
-        date_0 = cr.transform(dates[0]).columns.tolist()
-        date_1 = cr.transform(dates[1]).columns.tolist()
+        date_0 = cr.transform(real_estate_raw(0)).columns.tolist()
+        date_1 = cr.transform(real_estate_raw(1)).columns.tolist()
         assert set(date_0) - set(date_1) == {'batch_num', 'is_ad_active'}
 
 
 class TestTranslator:
 
-    @pytest.mark.parametrize('idx', [0, 1])
-    def test_wrong_column(self, idx, real_estate_data):
-        date = real_estate_data.directories[idx]
-        cr = ColumnRenamer(old_to_new=OLD_TO_NEW, hun_to_eng=HUN_TO_ENG)
-        df = cr.transform(real_estate_data.read(dir_name='raw', date=date))
+    def test_wrong_column(self, real_estate_renamed):
         t = Translator(column_name='wrong_column', hun_eng_map={})
         with pytest.raises(KeyError):
-            t.transform(df)
+            t.transform(real_estate_renamed)
+
+    def test_listing_type(self, real_estate_renamed):
+        to_be_translated = 'listing_type'
+        t = Translator(column_name=to_be_translated, hun_eng_map=LISTING_TYPE_HUN_TO_ENG)
+        translated_listing_type = t.transform(real_estate_renamed)[to_be_translated]
+        assert len(translated_listing_type.unique()) == 2
+        assert translated_listing_type.apply(lambda s: s not in ('elado', 'kiado')).all() == True
